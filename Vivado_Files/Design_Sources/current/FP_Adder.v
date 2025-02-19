fa1cab30f2c78877887fbde3b5bdd86b69f65c8e
prab7 <nicksingh1109@gmail.com>
2025-02-19 23:14:23 +0530

    Implentation: FA_Adder with Saturation logic

--- Vivado_Files/Design_Sources/current/FP_Adder.v
+++ Vivado_Files/Design_Sources/current/FP_Adder.v
@@ -19,7 +19,6 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module FP_Adder#(
     parameter SIGNED = 1,
     parameter INTEGER = 2,
@@ -29,11 +28,43 @@ module FP_Adder#(
     input[INTEGER+FRACTION-1:0] a,b,
     output reg [INTEGER+FRACTION-1:0] out
 );
-always @(*) begin
-    if(SIGNED==1) begin
-        out <= $signed(a) + $signed(b);
+
+        localparam TOTAL_WIDTH = INTEGER + FRACTION;
+
+    reg  [TOTAL_WIDTH:0] sum_raw;
+    reg [TOTAL_WIDTH-1:0] sum_saturated;
+
+    always @(*) begin
+        if (SIGNED==1)
+            sum_raw = $signed(a) + $signed(b);
+        else
+            sum_raw = a+b;
     end
-    else
-        out <= a+b;
-end
+    
+    always @(*) begin
+        if (SIGNED) begin
+
+            if (sum_raw[TOTAL_WIDTH] != sum_raw[TOTAL_WIDTH-1]) begin //overflow/underflow
+
+                if (sum_raw[TOTAL_WIDTH])
+                    sum_saturated = {1'b1, {TOTAL_WIDTH-1{1'b0}}}; //min
+                else
+                    sum_saturated = {1'b0, {TOTAL_WIDTH-1{1'b1}}}; //max
+
+            end else begin
+                sum_saturated = sum_raw[TOTAL_WIDTH-1:0];
+            end
+            
+        end else begin
+            if (sum_raw[TOTAL_WIDTH])
+                sum_saturated = {TOTAL_WIDTH{1'b1}}; //max
+            else
+                sum_saturated = sum_raw[TOTAL_WIDTH-1:0];
+        end
+    end
+
+    always @(*) begin
+        out = sum_saturated;
+    end
+
 endmodule
