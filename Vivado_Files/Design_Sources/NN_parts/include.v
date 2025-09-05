311bdd5d336878a80be92369d759659b80345ea2
Atul M <atul20040m@gmail.com>
2025-09-05 23:53:17 +0530

    Added missing Zynet hardware files for debug

---
+++ Vivado_Files/Design_Sources/NN_parts/include.v
@@ -0,0 +1,17 @@
+`define pretrained
+`define numLayers 4
+`define dataWidth 16
+`define numNeuronLayer1 64
+`define numWeightLayer1 169
+`define Layer1ActType "relu"
+`define numNeuronLayer2 32
+`define numWeightLayer2 64
+`define Layer2ActType "relu"
+`define numNeuronLayer3 10
+`define numWeightLayer3 32
+`define Layer3ActType "relu"
+`define numNeuronLayer4 10
+`define numWeightLayer4 10
+`define Layer4ActType "hardmax"
+`define sigmoidSize 10
+`define weightIntWidth 2
